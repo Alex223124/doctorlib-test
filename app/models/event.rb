8ed863158e60b2437a2577de35b2d0df85cce391
Alex223124 <sologoubalex@gmail.com>
2019-10-31 15:00:47 +0300

    kind validation added to Event, we have 2 kind of events in our system: opening - opens slots for appointments, and appointment - fills open slots

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,9 +1,12 @@
 class Event < ApplicationRecord
 
+  KINDS = ["opening", 'appointment'].freeze
+
   has_many :opening_slots, foreign_key: "opening_id", class_name: "Slot"
   has_many :appointment_slots, foreign_key: "appointment_id"
   has_many :booked_slots, through: :appointment_slots, source: :slot
 
   validates_presence_of :starts_at, :ends_at, :kind
+  validates :kind, inclusion: { in: KINDS, message: "%{value} is not valid. Should be one: #{KINDS.join(" OR ")}"  }
 
 end
