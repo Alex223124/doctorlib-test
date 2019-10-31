442aeaf4d3a2cf43a7ff8c915dc9280014c3b041
Alex223124 <sologoubalex@gmail.com>
2019-10-31 15:08:54 +0300

    wrapper for start_at attribute added

--- app/models/event.rb
+++ app/models/event.rb
@@ -11,6 +11,14 @@ class Event < ApplicationRecord
 
   validates_with Validators::Event::DateRange, if: :is_opening?
 
+  def starts_at_time
+    in_seconds(starts_at)
+  end
+
+  def in_seconds(time)
+    time.seconds_since_midnight.to_i
+  end
+
   def date_range
     ends_at - starts_at
   end
